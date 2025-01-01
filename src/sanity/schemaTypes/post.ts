430c5e9a136acd65ab81cb8b8bea085d9b33d7b0
Muhammad Saad Ahmed <147624130+saadahmedacademy@users.noreply.github.com>
2025-01-01 19:05:38 +0000

    complete the post schena,

--- src/sanity/schemaTypes/post.ts
+++ src/sanity/schemaTypes/post.ts
@@ -1,14 +1,45 @@
-export const postSchema = {
-      title: "Post Document",
-      name: "post",
-      type: "document",
-      fields: [
-        {
-          title: "Post Title",
-          name: "title",
-          type: "string"
-        }
-      ]
-    }  
-  
-  
+import { defineType,defineField, defineArrayMember } from 'sanity';
+
+export const postSchema = defineType({
+  title: 'Post Document',
+  name: 'post',
+  type: 'document',
+  fields: [
+   defineField({
+      title: 'Post Title',
+      name: 'title',
+      type: 'string',
+      description: 'Title of the post',
+      validation: (Rule) => Rule.required().min(10).max(80).error('Title should be between 10 to 80 characters'),
+    }),
+    defineField({
+      title: 'Post Slug',
+      name: 'slug',
+      type: 'slug',
+      description: 'Slug of the post',
+      validation: (Rule) => Rule.required().error('Slug is required'),
+      options: {
+        source: 'title',
+      },
+    }),
+    defineField({
+      title: 'Post Summary',
+      name: 'summary',
+      type: 'text',
+      validation: (Rule) => Rule.required().error('Summary is required'),
+    }),
+    defineField({
+      title: 'Post image',
+      name: 'image',
+      type: 'image',
+      validation: (Rule) => Rule.required().error('Image is required'),
+    }),
+    defineField({
+      title: 'Post Content',
+      name: 'content',
+      type: 'array',
+      of: [defineArrayMember({ type: 'block' })],
+      validation: (Rule) => Rule.required().error('Content is required'),
+    }),
+  ],
+});
